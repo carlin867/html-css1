1803be7a933a858e16d52b411cf9fc5dfa7644b8
carlin867 <140536486+carlin867@users.noreply.github.com>
2024-01-25 20:09:54 -0300

    atualizei 25/01/2024
    20:09

---
+++ modulos/Exercícios4/ex003/menu.js
@@ -0,0 +1,13 @@
+function menu() {
+  var menu = document.getElementById("menu-r");
+  var botao = document.getElementById("botao");
+  var img = document.getElementById("img-botao");
+  if (menu.style.display == "none") {
+    menu.style.display = "block";
+    botao.style.margin = "0px";
+    img.src = "close.svg";
+  } else {
+    menu.style.display = "none";
+    img.src = "menu.svg";
+  }
+}
